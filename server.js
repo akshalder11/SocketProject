1cc220448e878b4ee967edfdd92de15b355aac47
Akash <akshalder11@gmail.com>
2025-06-25 00:11:07 +0530

    Added Static Page to Project

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const http = require('http');
 const { Server } = require('socket.io');
 const connectDB = require('./db');
 const socketHandler = require('./socket');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: '*' }
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 connectDB().then(() => {
   socketHandler(io);
